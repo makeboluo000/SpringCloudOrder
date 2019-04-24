241f4070dd5c06aefcab2e7b54a19b79be481c4a
make <makeboluo000@163.com>
2019-04-24 17:12:22 +0800

    order测试提交1

---
+++ server/src/main/java/com/imooc/order/message/MqReceiver.java
@@ -0,0 +1,16 @@
+package com.imooc.order.message;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class MqReceiver {
+
+    @RabbitListener(queues = "myQueue")
+    public void process(String message) {
+      log.info("MqReceiver: {}" + message11);
+    }
+
+}
