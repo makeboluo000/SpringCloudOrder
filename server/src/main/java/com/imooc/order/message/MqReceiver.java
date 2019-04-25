77a404bba80d7bfc8a2ac3a6cc4f0ece355385d6
makeboluo000 <makeboluo000@163.com>
2019-04-25 23:39:08 +0800

    mq相关代码提交

--- server/src/main/java/com/imooc/order/message/MqReceiver.java
+++ server/src/main/java/com/imooc/order/message/MqReceiver.java
@@ -1,6 +1,9 @@
 package com.imooc.order.message;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +11,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class MqReceiver {
 
-    @RabbitListener(queues = "myQueue")
+//  1  @RabbitListener(queues = "myQueue") 只监听队列
+//  2  @RabbitListener(queuesToDeclare = @Queue("myQueue")) 自动创建并监听队列
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue("myQueue"),
+            exchange = @Exchange("myExchange")
+    ))
     public void process(String message) {
-      log.info("MqReceiver1: {}" + message);
+      log.info("MqReceiver1: {}", message);
     }
+
+    /**
+     * 服务2接受消息
+     * @param message
+     */
+    @RabbitListener(bindings = @QueueBinding(
+            exchange = @Exchange("myOrder"),
+            key = "computer",
+            value = @Queue("computerOrder")
+
+    ))
+    public void processComputer(String message) {
+        log.info("MqReceiver1: {}", message);
+    }
+
 
 }
