7ad5c08c18e5bf3a06e3eee7922f4458c4f4b62e
make <makeboluo000@163.com>
2019-05-15 16:18:40 +0800

    hystrix熔断提交

--- server/src/main/java/com/imooc/order/controller/HystrixController.java
+++ server/src/main/java/com/imooc/order/controller/HystrixController.java
@@ -4,6 +4,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,16 +14,29 @@ import java.util.Arrays;
 @DefaultProperties(defaultFallback = "defaultFallback")
 public class HystrixController {
 
-    @HystrixCommand(commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value = "3000")
-    })
+    // 超时配置 可以放在配置文件里面
+//    @HystrixCommand(commandProperties = {
+//            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+//    })
+    // 熔断相关设置 可以放在配置文件里面
+//    @HystrixCommand(commandProperties = {
+//            @HystrixProperty(name="circuitBreaker.enabled", value = "true"), // 是否开启熔断器
+//            @HystrixProperty(name="circuitBreaker.requestVolumeThreshold", value = "10"), // 请求数达到多少才进行百分比计算
+//            @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds", value = "10000"), // 半开试探休眠时间，默认值5000ms
+//            @HystrixProperty(name="circuitBreaker.errorThresholdPercentage", value = "60") //  设定错误百分比，默认值50% 达到这个错误比就进入回调函数
+//    })
+
+    @HystrixCommand
     @GetMapping("/getProductInfoList")
-    public String getProductInfoList() {
+    public String getProductInfoList(@RequestParam("num") Integer num) {
+        if(num % 2 == 0) {
+            return "我还活着呢";
+        }
+
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.postForObject("http://127.0.0.1:8082/product/listForOrder",
                 Arrays.asList("157875196366160022"),
                 String.class);
-
     }
 
     private String fallback() {
--- server/src/main/java/com/imooc/order/service/impl/OrderServiceImpl.java
+++ server/src/main/java/com/imooc/order/service/impl/OrderServiceImpl.java
@@ -18,7 +18,6 @@ import com.imooc.product.common.ProductInfoOutput;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
